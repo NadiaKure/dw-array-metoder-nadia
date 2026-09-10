b058960998beffff05ca094c353eaffa96dc50f5
Nadia <nadiaapoldkure@gmail.com>
2026-09-10 10:16:02 +0200

    løs opgave 3

--- arrays.js
+++ arrays.js
@@ -5,7 +5,18 @@ document.addEventListener("DOMContentLoaded", function() {
     let numbersArray = [12, 4, 45, 17, 2, 26, 7, 55, 48, 32, 21]
 
     // --- Løs opgaverne herunder --- 
+    //opgave 1
 let newNumbers = numbersArray.filter(number => number > 20);
 
 console.log("Opgave 1", newNumbers);
+
+//opgave 2
+catsArray.push("Sniffles");
+
+console.log("Opgave 2", catsArray);
+
+//opgave 3
+console.log("Opgave 3a", dogsArray.includes("Trixie"));
+
+console.log("Opgave 3b", dogsArray.includes("Baxter"));
 }) // ends DOMContentLoaded
